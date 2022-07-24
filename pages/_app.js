2e9189c97359818895b90fa6bbac04bbd3c7e42c
ayoubwazane <ayoubwazane306@gmail.com>
2022-07-24 02:15:23 +0100

    new updates and content

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/globals.scss'
-
+import "../styles/globals.scss";
+import styles from "../styles/Main.module.scss";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} styles={styles} />;
 }
 
-export default MyApp
+export default MyApp;
